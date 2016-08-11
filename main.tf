27414fc6bc06c7ce5ad5600aeef551f9ba218f3f
Mike Peachey <mike.peachey@bjss.com>
2016-08-12 00:03:46 +0100

    Module env in bootstrap

---
+++ main.tf
@@ -0,0 +1,12 @@
+# This is a temporary way to invoke the environment using the bootstrap module.
+# This in future will be run as an explicitly seperate Jenkins slave job, not invoked here
+module "environment" {
+  source                          = "github.com/Zordrak/terraform-environment.git"
+  aws_region                      = "${var.aws_region}"
+  vpc_id                          = "${aws_vpc.vpc.id}"
+  environment                     = "${var.environment}"
+  project                         = "${var.project}"
+  region_az_count                 = "3"
+  internet_gateway_id             = "${aws_internet_gateway.internet_gateway.id}"
+  bootstrap_public_route_table_id = "${aws_route_table.public.id}"
+}
