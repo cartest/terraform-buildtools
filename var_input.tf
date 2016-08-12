5926dd80dc21bd92a036f023aa0a43726f7482d8
Mike Peachey <mike.peachey@bjss.com>
2016-08-12 11:05:51 +0100

    peering input

--- var_input.tf
+++ var_input.tf
@@ -1,25 +1,7 @@
-variable "vpc_cidr" {
+variable "aws_account_id" {
   type        = "string"
-  default     = "10.0.0.0/16"
-  description = "VPC CIDR Block"
-}
-
-variable "private_dns_domain" {
-  type        = "string"
-  default     = "environment.project.platform.client"
-  description = "Private Hosted Zone domain name for the VPC"
-}
-
-variable "environment" {
-  type        = "string"
-  default     = "ENVIRONMENT"
-  description = "Environment Name"
-}
-
-variable "project" {
-  type        = "string"
-  default     = "PROJECT"
-  description = "Project/Application Name"
+  default     = "000000000000"
+  description = "AWS Account ID"
 }
 
 variable "aws_region" {
@@ -28,8 +10,38 @@ variable "aws_region" {
   description = "AWS Region"
 }
 
+variable "environment" {
+  type        = "string"
+  default     = "ENVIRONMENT"
+  description = "Environment Name"
+}
+
+variable "peer_vpcs" {
+  type        = "list"
+  default     = []
+  description = "List of VPCs to create/own peering connections with"
+}
+
+variable "private_dns_domain" {
+  type        = "string"
+  default     = "environment.project.platform.client"
+  description = "Private Hosted Zone domain name for the VPC"
+}
+
+variable "project" {
+  type        = "string"
+  default     = "PROJECT"
+  description = "Project/Application Name"
+}
+
 variable "region_az_count" {
   type        = "string"
   default     = "3"
   description = "An ugly hack"
+}
+
+variable "vpc_cidr" {
+  type        = "string"
+  default     = "10.0.0.0/16"
+  description = "VPC CIDR Block"
 }
